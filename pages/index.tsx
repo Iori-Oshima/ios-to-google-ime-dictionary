7d9905402996b910750eb391f0ce17a91838864a
Iori-Oshima <iorin619@gmail.com>
2025-03-20 20:31:50 +0900

    fix: change text

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">iOS辞書 → Google日本語入力変換</h1>
+      <h1 className="text-2xl font-bold mb-4">Apple辞書 → Google日本語入力変換</h1>
       <FileUploader onFileSelect={setFile} />
       {file && <PlistConverter file={file} />}
     </div>
