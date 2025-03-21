a0759028f309dcb80730758e76f545f459eb900b
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2025-03-21 15:39:08 +0900

    chore: update config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+const repoName = 'ios-to-google-ime-dictionary';
+const basePath = isProduction ? `/${repoName}` : '';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
+  basePath: basePath,
 };
 
 export default nextConfig;
